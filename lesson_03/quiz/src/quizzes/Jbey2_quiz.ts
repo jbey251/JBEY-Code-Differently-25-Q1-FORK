ab051de673d3fe13bd3f9226c562b76d49444022
John <jbey251@gmail.com>
2026-03-10 18:53:52 +0000

    Bad Branch 4

--- lesson_03/quiz/src/quizzes/Jbey2_quiz.ts
+++ lesson_03/quiz/src/quizzes/Jbey2_quiz.ts
@@ -6,7 +6,7 @@ import {
 } from 'codedifferently-instructional';
 export class JBey2Quiz implements QuizQuestionProvider {
   getProviderName(): string {
-    return 'JBey1', 'Bad3';
+    return 'JBey1', 'Bad4';
   }
   makeQuizQuestions() {
     return [
