cd30f944dfe5bbc89b3a8a89bdaeea4d99851004
John <jbey251@gmail.com>
2026-03-10 18:10:26 +0000

    testing rebase

--- lesson_03/quiz/src/quizzes/Jbey2_quiz.ts
+++ lesson_03/quiz/src/quizzes/Jbey2_quiz.ts
@@ -6,7 +6,7 @@ import {
 } from 'codedifferently-instructional';
 export class JBey2Quiz implements QuizQuestionProvider {
   getProviderName(): string {
-    return 'JBey2';
+    return 'JBey1', 'test';
   }
   makeQuizQuestions() {
     return [
