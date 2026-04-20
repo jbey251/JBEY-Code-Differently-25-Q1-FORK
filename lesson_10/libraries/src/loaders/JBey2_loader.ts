02a7e05e95aa9702c36a49f0b0515f0593258b31
John <jbey251@gmail.com>
2026-04-20 14:15:38 +0000

    FEAT: Implemented extra credit that combines the credits with the media items through the loadData method.

--- lesson_10/libraries/src/loaders/JBey2_loader.ts
+++ lesson_10/libraries/src/loaders/JBey2_loader.ts
@@ -12,6 +12,24 @@ export class JBeyLoader2 implements Loader {
     const credits = await this.loadCredits();
     const mediaItems = await this.loadMediaItems();
 
+    // Creates a Map using each media item's ID as the key. This makes it easy
+    // to look up a specific media item by ID later - Instead of looping through
+    // the whole array every time.
+    const mediaMap = new Map<string, MediaItem>();
+    for (const media of mediaItems) {
+      mediaMap.set(media.getId(), media);
+    }
+
+    // Loops through every credit, finds which media item
+    // t belongs to using getMediaItemId(), and adds that
+    // credit to the media item with addCredit().
+    for (const credit of credits) {
+      const mediaItem = mediaMap.get(credit.getMediaItemId());
+      if (mediaItem) {
+        mediaItem.addCredit(credit);
+      }
+    }
+
     console.log(
       `Loaded ${credits.length} credits and ${mediaItems.length} media items`,
     );
