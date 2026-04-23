64652da9bb7f95942de0e96df5e2c955bd81d062
John <jbey251@gmail.com>
2026-04-23 16:02:26 +0000

    CHORE: Implement getConcatenation method logic for ts file.

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,8 +3,21 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+  let n = nums.length;
+  let ans = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+  
+  return ans;
 }
+
+console.log(getConcatenation([1, 2]));
 
 /**
  * Provide the solution to LeetCode 2942 here:
