a93320565cc59f7ee681df1c83b36b5c4bc8b79d
John <jbey251@gmail.com>
2026-04-23 18:00:45 +0000

    CHORE: Redid the getConcatentation method logic to include the for-of loop.

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,17 +3,14 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  let ans = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    ans.push(nums[i]);
+  let array = [];
+  for (const ans of nums) {
+    array.push(ans);
   }
-
-  for (let i = 0; i < nums.length; i++) {
-    ans.push(nums[i]);
+  for (const ans of nums) {
+    array.push(ans);
   }
-  
-  return ans;
+  return array;
 }
 
 console.log(getConcatenation([1, 2]));
