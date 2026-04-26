b2bb20040c991c12bbf8abe8fd7ba9276a7cc953
John <jbey251@gmail.com>
2026-04-26 02:23:31 +0000

    FEAT: Implement lesson12 class logic for the gameResult() method in the Lesson12.java file, along with Stack class logic for each method in the Stack.java file. To-Do ts files, & also go over java files.

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,36 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    ListNode evenNode = head;
+    ListNode oddNode = head.next;
+    int oddTeamScore = 0;
+    int evenTeamScore = 0;
+
+    while (evenNode != null) {
+      int evenVal = evenNode.val;
+      int oddVal = oddNode.val;
+
+      if (evenVal > oddVal) {
+        evenTeamScore++;
+      } else if (oddVal > evenVal) {
+        oddTeamScore++;
+      } else {
+        evenTeamScore++;
+        oddTeamScore++;
+      }
+
+      evenNode = evenNode.next.next;
+      if (evenNode != null) {
+        oddNode = evenNode.next;
+      }
+    }
+
+    if (evenTeamScore == oddTeamScore) {
+      return "Tie";
+    } else if (evenTeamScore > oddTeamScore) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
   }
 }
